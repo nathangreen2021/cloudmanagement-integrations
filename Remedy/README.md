b0d9fe181d7d05c81bdef03d74ecdbd8449b7d51
scottybullet <scottybullet@gmail.com>
2022-02-12 10:08:11 -0500

    Add Remedy - Open Work order Sample

--- Remedy/README.md
+++ Remedy/README.md
@@ -1,18 +1,26 @@
 # Commander Integrations - Remedy
 
-Completion Module to Close a work order in remedy
+Completion Module to Close a work order in remedy and Create Work Order Module (Sample)
 
 Requirements: 
 * Commander 8.6.0 or higher
 * Advanced property "embotics.workflow.script.credentials" must be set to "true"
 * BMC Helix ITSM: Service Desk 21.05(as tested, may work with other versions)
 
-Module Installation and Setup:
+Close Work Order Module Installation and Setup:
 1. Ensure a credential exists in "Configuration > Credentials" for Remedy 
 2. Import the VM Completion Module for closing a work order
 2. Open the Module and specify the credential to Connect to Remedy. 
 3. Add the module to your Completion workflow specifying the Required parameters for your setup.
 4. If the Module fails to run, the comments will contain the errors 
+
+Create Work Order Module (Sample) Installation and Setup:
+1. Ensure a credential exists in "Configuration > Credentials" for Remedy 
+2. Import the VM Completion Module for closing a work order
+2. Open the Module and specify the credential to Connect to Remedy. 
+3. Add the module to your Completion workflow specifying the Required parameters for your setup.
+4. If the Module fails to run, the comments will contain the errors 
+
 
 Note: not all Remedy status transitions are acceptable: 
 * https://docs.bmc.com/docs/srm81/about-work-order-status-transitions-225509555.html
--- Remedy/Remedy-Open_WorkOrder_Completion_Module.ps1
+++ Remedy/Remedy-Open_WorkOrder_Completion_Module.ps1
@@ -1,24 +1,37 @@
 <#
-Module to change the Workorder State in Remedy as part of a VM Completion Workflow.
+Sample Module to Create a Workorder in Remedy as part of a VM Completion Workflow.
 
 Requirements: 
 * Commander 8.6.0 or higher
 * Advanced property "embotics.workflow.script.credentials" must be set to "true"
-
 #>
+
 $RemedyAddress = "#{inputVariable['remedy_address']}"
-$Username = (Get-Item Env:SELECTED_CREDENTIALS_USERNAME ).value;   #Credential
+$Username = (Get-Item Env:SELECTED_CREDENTIALS_USERNAME ).value; #Credential
 $Password = (Get-Item Env:SELECTED_CREDENTIALS_PASSWORD).value;
-$WorkorderID = "#{inputVariable['RemedyWOiD']}" 
-$Status = "#{inputVariable['workOrderStatus']}"     # Status to set the work order to
 $BypassCert = "#{inputVariable['bypass_Cert']}"    #to bypass an unsigned SSL cert on the Target
 
+#Workorder Variables
+$Description = "Created by Snow Commander"
+$Firstname = "XXXX"
+$Lastname = "XXXX"
+$CustomerFirstName = "XXXX"
+$CustomerLastName = "XXXX"
+$Summary = "Commander RequestID - #{request.id}"
+$Status = "#{inputVariable['workOrderStatus']}" 
+$Company = "#{request.requester.organization.name}"
+$CustomerCompany = "#{request.requester.organization.name}"
+$Location = "#{request.requester.organization.name}"
 
 #$DebugPreference="Continue"
 $ErrorActionPreference = "Stop"
 
 if (!($RemedyAddress) -or !($WorkorderID) -or !($UserName) -or !($Password)) {
-    Write-error "Please provide Remedy information to close the workorder"
+    Write-error "Please provide Remedy Login Information"
+    Exit 1
+}
+if (!($Description) -or !($Firstname) -or !($Lastname) -or !($CustomerFirstName) -or !($CustomerLastName) -or !($Summary) -or !($Status) -or !($Company) -or !($CustomerCompany) -or !($Location)) {
+    Write-error "Please provide Remedy Login Information"
     Exit 1
 }
 
@@ -28,7 +41,7 @@ if ($BypassCert -eq "Yes") {
 
     # Create a compilation environment
     $Provider = New-Object Microsoft.CSharp.CSharpCodeProvider
-    $Compiler = $Provider.CreateCompiler()
+    $Provider.CreateCompiler()
     $Params = New-Object System.CodeDom.Compiler.CompilerParameters
     $Params.GenerateExecutable = $False
     $Params.GenerateInMemory = $True
@@ -61,45 +74,27 @@ if ($BypassCert -eq "Yes") {
         password = $($Password)
     }
     $Token = Invoke-RestMethod -uri $TokenUri -Method POST -ContentType $TokenContentType -body $TokenBody
-
-#Get Remedy WorkOrders
-    $WoUri = "https://$($RemedyAddress)/api/arsys/v1/entry/WOI:WorkOrderInterface"
     $AuthHeader = @{
         Authorization = "AR-JWT $($Token)"
     }
-    $WoResults = Invoke-RestMethod -uri $WoUri -Method GET -ContentType 'application/json' -Headers $AuthHeader
 
-    $WoData = $WoResults.entries.values| Select-object "Request ID","Work Order ID", Status | where-object "Work Order ID" -eq $WorkorderID
-    $WoReqID = $WoData."Request ID"
-
-
-#Close Workorder if it's not in a closed state
-    $woStatus = $WoData.status
-    if ($woStatus -eq "$status") {
-        Write-host "WorkOrder number $WorkorderID is already closed"
-        Exit 0  
-    }
-    Elseif ($woStatus -ne "$Status") {
-        Write-host "WorkOrder $WorkorderID is $woStatus Attempting to close. "
-        $WoclUri = "https://$($RemedyAddress)/api/arsys/v1/entry/WOI:WorkOrderInterface/$WoReqID"
-        $Wojsonbody  = "{""values"":
-                            { ""Status"" : ""$status"" }
-                        }"
+#Create Workorder
+        $WoclUri = "https://$($RemedyAddress)/api/arsys/v1/entry/WOI:WorkOrderInterface_Create/"
+        $Wojsonbody = "{""values"": {
+                        ""Detailed Description"": ""$Description"",
+                        ""z1D_Action"": ""CREATE"",
+                        ""First Name"": ""$firstname"",
+                        ""Last Name"": ""$lastname"",
+                        ""Customer First Name"": ""$CustomerFirstName"",
+                        ""Customer Last Name"": ""$CustomerLastName"",
+                        ""Summary"": ""$Summary"",
+                        ""Status"": ""$Status"",
+                        ""Company"": ""$Company"",
+                        ""Customer Company"": ""$CustomerCompany"",
+                        ""Location Company"": ""$Location""
+                        }
+        }"
         $Wojsonbody = ConvertFrom-Json $Wojsonbody| ConvertTo-Json 
-        Invoke-RestMethod -uri $WoclUri -Method PUT -ContentType 'application/json' -Headers $AuthHeader -Body $Wojsonbody
-    #Check State
-        $WoResults = Invoke-RestMethod -uri $WoUri -Method GET -ContentType 'application/json' -Headers $AuthHeader
-        $WoData = $WoResults.entries.values | Select-object "Request ID", "Work Order ID", Status | where-object "Work Order ID" -eq $WorkorderID
-        $WoState = $WoData.Status
-        if($wostate -eq $status){
-            Write-host "WorkOrder $WorkorderID has been Marked as $status"
-        }
-        Exit 0
-    }
-    Else {
-        Write-Error "Something went wrong looking up the status for WorkorderID $WorkorderID, Please contact your Remedy Administrator"
-        Exit 1
-        
-    } 
+        Invoke-RestMethod -uri $WoclUri -Method POST -ContentType 'application/json' -Headers $AuthHeader -Body $Wojsonbody
 
- 
+    
